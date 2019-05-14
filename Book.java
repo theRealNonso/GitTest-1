d1bdd700fd3074dcfc162d4cc2c4475b8f03a0b9
tosine6 <adetosine6@gmail.com>
2019-05-14 16:08:16 +0100

    Second commit

--- Book.java
+++ Book.java
@@ -7,6 +7,19 @@ public class Book{
     private String chapters;
     private int pages;
 
-    
+        public void setAuthor(String author){
+            this.author = author;
+        }
+        public String getAuthor (){
+            return author;
+        }
+
+        public void setTitle (String title){
+            this.title = title;
+        }
+
+        public String getTitle (){
+            return title;
+        }
     }
 }
